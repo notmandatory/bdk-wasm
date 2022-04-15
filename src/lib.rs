f89c138d36d2de059097d55e16418e81fcf11caa
Steve Myers <steve@notmandatory.org>
2022-04-14 20:17:27 -0700

    Add WalletWrapper and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,20 @@
-mod utils;
+use bdk::bitcoin::Network;
+use bdk::blockchain::EsploraBlockchain;
+use bdk::database::MemoryDatabase;
+use bdk::wallet::AddressIndex;
+use bdk::{SyncOptions, Wallet};
+use std::collections::HashMap;
+use std::rc::Rc;
+use std::str::FromStr;
 
+use js_sys::Promise;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen_futures::{future_to_promise, spawn_local};
+
+use log::{debug, info};
+use serde::Deserialize;
+
+mod utils;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,8 +23,89 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
+}
+
+//#[wasm_bindgen]
+//pub fn init() {
+//    console_log::init_with_level(log::Level::Debug).unwrap();
+//    utils::set_panic_hook();
+//
+//    info!("Initialization completed");
+//}
+
+#[wasm_bindgen]
+pub struct WalletWrapper {
+    wallet: Rc<Wallet<MemoryDatabase>>,
+    blockchain: Rc<EsploraBlockchain>,
+}
+
+#[wasm_bindgen]
+impl WalletWrapper {
+    #[wasm_bindgen(constructor)]
+    pub async fn new(
+        network: String,
+        descriptor: String,
+        change_descriptor: Option<String>,
+        esplora: String,
+        stop_gap: usize,
+    ) -> Result<WalletWrapper, String> {
+        let network = match network.as_str() {
+            "regtest" => Network::Regtest,
+            "testnet" | _ => Network::Testnet,
+        };
+
+        debug!(
+            "descriptor: {:?}\nchange descriptor {:?}",
+            descriptor, change_descriptor
+        );
+
+        let blockchain = EsploraBlockchain::new(&esplora, stop_gap);
+        let wallet = Wallet::new(
+            descriptor.as_str(),
+            change_descriptor.as_ref().map(|x| x.as_str()),
+            network,
+            MemoryDatabase::new(),
+        )
+        .map_err(|e| format!("{:?}", e))?;
+
+        Ok(WalletWrapper {
+            wallet: Rc::new(wallet),
+            blockchain: Rc::new(blockchain),
+        })
+    }
+
+    pub fn sync(&self) -> Promise {
+        let wallet = Rc::clone(&self.wallet);
+        let blockchain = Rc::clone(&self.blockchain);
+        future_to_promise(async move {
+            wallet
+                .as_ref()
+                .sync(blockchain.as_ref(), SyncOptions::default())
+                .await
+                .map_err(|e| format!("{:?}", e))?;
+            Ok("done".into())
+        })
+    }
+
+    #[wasm_bindgen]
+    pub fn balance(&self) -> Result<u64, String> {
+        let balance = self.wallet.get_balance().map_err(|e| format!("{:?}", e))?;
+        info!("balance: {}", &balance);
+        Ok(balance)
+    }
+
+    #[wasm_bindgen]
+    pub fn get_new_address(&self) -> Result<String, String> {
+        let new_address = self
+            .wallet
+            .get_address(AddressIndex::New)
+            .map_err(|e| format!("{:?}", e))?
+            .address
+            .to_string();
+        Ok(new_address)
+    }
 }
 
 #[wasm_bindgen]
--- tests/web.rs
+++ tests/web.rs
@@ -3,12 +3,43 @@
 #![cfg(target_arch = "wasm32")]
 
 extern crate wasm_bindgen_test;
+use bdk_wasm::WalletWrapper;
 use wasm_bindgen_test::*;
-use bdk_wasm::greet;
 
 wasm_bindgen_test_configure!(run_in_browser);
 
+async fn test_wallet() -> Result<WalletWrapper, String> {
+    let network = "testnet".to_string();
+    let descriptor = "wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/0/*)".to_string();
+    let change_descriptor = Some("wpkh([c258d2e4/84h/1h/0h]tpubDDYkZojQFQjht8Tm4jsS3iuEmKjTiEGjG6KnuFNKKJb5A6ZUCUZKdvLdSDWofKi4ToRCwb9poe1XdqfUnP4jaJjCB2Zwv11ZLgSbnZSNecE/1/*)".to_string());
+    let esplora = "https://blockstream.info/testnet/api".to_string();
+    WalletWrapper::new(network, descriptor, change_descriptor, esplora, 20).await
+}
+
 #[wasm_bindgen_test]
-fn pass() {
-    assert_eq!(1 + 1, 2);
+async fn test_sync() {
+    let wallet = test_wallet().await.expect("wallet");
+    let promise = wallet.sync();
+    let result = wasm_bindgen_futures::JsFuture::from(promise).await.expect("result");
+    assert_eq!(result.as_string().unwrap(), "done".to_string());
+
+    let balance = wallet.balance().expect("balance");
+    assert!(balance > 0);
+}
+
+#[wasm_bindgen_test]
+async fn test_balance() {
+    let wallet = test_wallet().await.expect("wallet");
+    let balance = wallet.balance().expect("balance");
+    assert_eq!(balance, 0);
+}
+
+#[wasm_bindgen_test]
+async fn test_new_address() {
+    let wallet = test_wallet().await.expect("wallet");
+    let new_address = wallet.get_new_address().expect("new_address");
+    assert_eq!(
+        new_address,
+        "tb1qzg4mckdh50nwdm9hkzq06528rsu73hjxxzem3e".to_string()
+    );
 }
